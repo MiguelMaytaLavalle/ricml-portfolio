d04026aee2a6282f18d28c271f4b7d6627cfb229
ricml <48988098+MiguelMaytaLavalle@users.noreply.github.com>
2021-04-26 01:11:32 +0200

    init portfolio

---
+++ src/components/header/HeaderComponent.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import 'font-awesome/css/font-awesome.min.css'
+import "./HeaderComponent.css"
+import Logo from '../icon/ricml-logo-v7.png'
+
+class HeaderComponent extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipient: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange() {
+    }
+
+    handleCallback = (autoSuggest) => {
+    }
+
+    render(){
+        return(
+            <header className="site-header">
+                <section className="top-header-area">
+                    <div className="inner-header content">
+                        <div className="about-area">
+                            <a href="/about">About</a>
+                        </div>
+                        <div className="logo-area">
+                            <img className="logo-image" src={Logo}/>
+                        </div>
+                        <div className="social-area">
+                            <ul>
+                                <li><a href="https://github.com/MiguelMaytaLavalle"><i className="fa fa-github fa-3x"></i></a></li>
+                                <li><a href="https://www.linkedin.com/in/ricardo-mayta-lavalle-209665182/"><i className="fa fa-linkedin fa-3x"></i></a></li>
+                                <li><a href="/contact"><i className="fa fa-envelope-o fa-3x"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            </header>
+        );
+    }
+}
+
+export default (HeaderComponent)
---
+++ src/components/viewcontroller/ViewController.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import HeaderComponent from "../header/HeaderComponent";
+import "./ViewController.css"
+import Forest from "../video/forest-background.mp4";
+
+class ViewController extends Component{
+    render(){
+        return(
+            <div className="App">
+                <HeaderComponent/>
+                <div className="hero-overlay-home">
+                    <video className="video-overlay" autoPlay loop muted>
+                        <source src={Forest}/>
+                    </video>
+                    <div className="hero-overlay"></div>
+                </div>
+                <div className="intro-area">
+                    <section className="intro-text">
+                        <h1>Computer Engineering Student</h1>
+                    </section>
+                </div>
+                <section>
+                    <grid></grid>
+                </section>
+            </div>
+        );
+    }
+}
+export default ViewController
