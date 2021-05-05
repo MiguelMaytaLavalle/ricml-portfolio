a285c7a8849fdb97a6b7dcdba7321d3ee49e0f75
ricml <48988098+MiguelMaytaLavalle@users.noreply.github.com>
2021-05-05 14:20:11 +0200

    3

--- src/components/viewcontroller/ViewController.js
+++ src/components/viewcontroller/ViewController.js
@@ -8,6 +8,8 @@ import {fadeInLeft, fadeInDown, fadeIn} from 'react-animations';
 import CardComponent from "../card/CardComponent";
 import ScrollAnimation from 'react-animate-on-scroll'
 import CarouselProjectsComponent from "../carousel/CarouselProjectsComponent";
+import Sheep from '../image/sheep.jpg'
+import Cursed from '../image/Cursed.jpg'
 
 const fadeInLeftAnimation = keyframes`${fadeInLeft}`;
 const fadeInDownAnimation = keyframes`${fadeInDown}`;
@@ -47,6 +49,72 @@ class ViewController extends Component {
                     </div>
                 </div>
 
+                <div className="page" style={{background: "black"}}>
+                    <div className="box">
+                        <div className="ib">
+                            <h1>ABOUT ME</h1>
+                            <ScrollAnimation animateIn="bounceInLeft">
+                                <p>My name is <span className="highlight"> Ricardo Mayta Lavalle</span> and I'm a <span className="highlight"> Computer Engineer Student </span>at
+                                    <br/>Royal Institute of Technology in Stockholm.
+                                    <br/>Currently I'm planning to <span className="highlight"> graduate</span> in <span className="highlight">2021</span>.
+                                </p>
+                                <h4>Tools & Technology</h4>
+                                <p>Languages: Python, Java, C, Node.JS</p>
+                                <p>React, </p>
+
+                            </ScrollAnimation>
+                        </div>
+                        <div className="ib">
+                            <div className="container">
+                                    <img src={Cursed} alt="selfie 2021"/>
+                            </div>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="page" style={{background: "black"}}>
+                    <div className="box">
+                        <div className="ib">
+                            <h1>ABOUT ME</h1>
+                            <p>My name is Ricardo Mayta Lavalle and I'm a Computer Engineer Student at Royal Institute of Technology.
+                                Currently I'm planning to graduate in 2021.
+                            </p>
+                        </div>
+                        <div className="ib">
+                            <div className="container">
+                                <img src={Sheep} alt="selfie 2021"/>
+                            </div>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="page" style={{background: "black"}}>
+
+                    <ScrollAnimation animateIn="fadeIn">
+                        <div className="about-area">
+                            <section className="about">
+                                <h1 className="display-4">ABOUT</h1>
+                            </section>
+                            <div className="rowC">
+                                <div className="ib">
+                                    <h1>Hello!</h1>
+                                    <p>My name is <span className="highlight">Ricardo Mayta Lavalle</span> </p>
+                                    <p>I'm a Computer Engineer Student at Royal Institute of Technology in Stockholm, Sweden.</p>
+                                    <p>Currently I'm planning to graduate in 2021.</p>
+                                </div>
+                                <div className="about-image">
+                                    <h1>Image</h1>
+
+                                </div>
+                            </div>
+                        </div>
+                    </ScrollAnimation>
+
+                </div>
+
+
 
                 <div className="page" style={{background: "black"}}>
 
@@ -57,27 +125,25 @@ class ViewController extends Component {
                             </section>
                         </div>
                     </ScrollAnimation>
-
-                    <CarouselProjectsComponent/>
                 </div>
-
 
                 <div className="page" style={{background: "black"}}>
-
-                    <ScrollAnimation animateIn="fadeIn">
-                        <div className="project-area">
-                            <section className="project-text-section">
-                                <h1 className="display-4">ABOUT</h1>
-                                <div className="about-text">
-                                    <ScrollAnimation animateIn="bounceInLeft">
-                                        <h1>Hej</h1>
-                                    </ScrollAnimation>
-                                </div>
-                            </section>
+                    <div className="rowC">
+                        <div className="project-title">
+                            <h1>
+                                ScanSmart
+                            </h1>
+                            <p>A self-scanning tool that will give users recommendations on how to improve their
+                                environmental impact when grocery shopping based on what is in their shopping cart.</p>
                         </div>
-                    </ScrollAnimation>
-
+                        <div className="project-image">
+                            <h1>
+                                Image
+                            </h1>
+                        </div>
+                    </div>
                 </div>
+
 
             </div>
         );
